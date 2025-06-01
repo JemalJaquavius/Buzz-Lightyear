435c9471e357525c8a5616304291a00efedd879a
JemalJaquavius <acrause@learn68.ca>
2025-06-01 11:33:05 -0700

    Update AndyInformation.js

--- AndyInformation.js
+++ AndyInformation.js
@@ -1,3 +1,10 @@
 function informationOnAndy() {
   document.getElementById("note").innerHTML = "Andy is a human, not a toy."
 }
+let x = 10;
+let y = 15;
+if (x < y) {
+function theTruth() {
+  document.getElementById("Woody sucks").innerHTML = "Woody is not as good as Buzz Lightyear"
+  }
+}
