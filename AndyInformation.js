248ec28fc2119cc8d01b948bd7a0364fecb76707
JemalJaquavius <acrause@learn68.ca>
2025-06-03 12:38:54 -0700

    Update AndyInformation.js

--- AndyInformation.js
+++ AndyInformation.js
@@ -1,10 +1,15 @@
 function informationOnAndy() {
-  document.getElementById("note").innerHTML = "Andy is a human, not a toy."
+  document.getElementById("note").innerHTML = "Andy is a human, not a toy.";
 }
 let x = 10;
 let y = 15;
 if (x < y) {
 function theTruth() {
-  document.getElementById("Woody sucks").innerHTML = "Woody is not as good as Buzz Lightyear"
+  document.getElementById("Woody sucks").innerHTML = "Woody is not as good as Buzz Lightyear";
   }
+}
+const l = "Buzz Lightyear is better than every character in that exists";
+
+function theBigTruth() {
+  document.getElementById("theBestTruth").innerHTML = l;
 }
